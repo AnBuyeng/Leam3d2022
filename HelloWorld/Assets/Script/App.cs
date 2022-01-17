3822c014d6da1d54f593eb0d9213c8070453bb2b
DESKTOP-PF8JT66\com <qudcks004@naver.com>
2022-01-17 14:54:41 +0900

    백터 덧샘과 뺄샘

--- HelloWorld/Assets/Script/App.cs
+++ HelloWorld/Assets/Script/App.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class App : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HelloWorld");
+        Vector2 playerPos = new Vector2(2, 3);
+        Vector2 monsterPos = new Vector2(5, 8);
+
+        var dir = monsterPos - playerPos;
+        var distance = dir.magnitude;
+        Debug.Log(distance);
+
     }
 
     // Update is called once per frame
